30c6374dd6964a1627a405f83b50ac6d6f14b288
Santhu489 <100673668+Santhu489@users.noreply.github.com>
2024-02-04 18:50:15 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
+pip install --upgrade pandas
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 from imblearn.over_sampling import RandomOverSampler
+# For Windows
+python -m venv venv
 
 genes = pd.read_csv("sfari_genes.csv")
 
